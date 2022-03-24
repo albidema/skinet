88ff69feabc269c6c6045881414c9d21aed3f414
Albi DEMA <a.dema@atrax.al>
2022-03-24 17:53:09 +0100

    identity validation

--- API/Dtos/AddressDto.cs
+++ API/Dtos/AddressDto.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 namespace API.Dtos
 {
     public class AddressDto
-    {      
+    {
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Street { get; set; }
